d53d32b031344ed9ccf55d2986b09478d7e7fc20
AlexNisnevich <alex.nisnevich@gmail.com>
2010-09-25 20:47:07 -0700

    Further expanding prototype

--- rickremoval-new.js
+++ rickremoval-new.js
@@ -1,12 +1,25 @@
+
+var blocklist;
+var whitelist;
+var RICKROLL_THRESHOLD = 20;
+var url = window.location.href;
+var body = document.getElementsByTagName('BODY')[0];
+var html = body.innerHTML;
+
 initArrays();
 
-(initialize variables)
 
-var body = document.getElementsByTagName('BODY')[0];
 
 if (getStrictness()) { // Strict check
-	if (strictCheck()) {
+	if (looseCheck()) {
 		disablePage();
+		showStrictPopup();
+	} else {
+		var matches = strictCheck();
+		if (matches >= RICKROLL_THRESHOLD) {
+			disablePage();
+			showLoosePopup(matches);
+		}
 	}
 } else { // Loose check
 	if (!inWhiteList() && looseCheck()) {
@@ -14,10 +27,57 @@ if (getStrictness()) { // Strict check
 	}
 }
 
+
+
 function getStrictness() {
-	return (window.location.href.search("youtube.com/watch") != -1);
+	// For now, only youtube is treated strictly
+	return (url.search("youtube.com/watch") != -1);
 }
+
+
+
+function strictCheck() {
+	// heuristic check, return # of matches
+}
+
+
+
+function looseCheck() {
+	return testAgainstArray(url, blocklist);
+}
+
+function inWhiteList() {
+	return testAgainstArray(url, whitelist);
+}
+
+function testAgainstArray (text, array) {
+	// array-match check, return 1 if match found, 0 otherwise
+}
+
+
+
+function showStrictPopup(matches) {
+	// display popup (mention # of matches)
+	// if user hits OK, call enablePage()
+}
+
+function showLoosePopup() {
+	// display popup
+	// if user hits OK, call enablePage()
+}
+
+
 
 function disablePage() {
 	body.innerHTML = "";
+}
+
+function enablePage() {
+	body.innerHTML = html;
+}
+
+
+
+function initArrays() {
+
 }
